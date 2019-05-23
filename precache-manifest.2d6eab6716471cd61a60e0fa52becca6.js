1f321b66cb0b9c6e9de38a1bbae1ac2723dba9b8
Maybol <maybol.huracahua@tecsup.edu.pe>
2019-05-23 15:14:02 -0500

    first commit

---
+++ precache-manifest.2d6eab6716471cd61a60e0fa52becca6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "433be22c81182bb670c46ca1a3edef47",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ecc6fcb744f25668dece",
+    "url": "/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "2f31b4ccf299a63fefc5",
+    "url": "/static/js/2.32cc53d5.chunk.js"
+  },
+  {
+    "revision": "ecc6fcb744f25668dece",
+    "url": "/static/js/main.fb085e93.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
